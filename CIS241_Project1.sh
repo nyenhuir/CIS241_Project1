27989a3e32cce2269c4798b2698d9f7b3d1f9847
Reuben Nyenhuis <nyenhuir@eos07.cis.gvsu.edu>
2019-06-25 14:12:36 -0400

    First upload 6-25

--- CIS241_Project1.sh
+++ CIS241_Project1.sh
@@ -4,8 +4,10 @@ myip=$(curl 'https://api.ipify.org?format=json' | cut -d: -f 2 | tr -d /} | tr -
 mylatitude=$(curl https://ipvigilante.com/$myip | jq '.data.latitude' | tr -d '"')
 mylongitude=$(curl https://ipvigilante.com/$myip | jq '.data.longitude' | tr -d '"')
 forecasturl=$(curl -L https://api.weather.gov/points/$mylatitude,$mylongitude | jq '.properties.forecast' | tr -d '"')
-firstperiod=$(curl -L $forecasturl | jq '.properties.periods[0].name' | tr -d '"')
-detailedForecast=$(curl -L $forecasturl | jq '.properties.periods[0].detailedForecast' | tr -d '"')
+forecast=$(curl -L $forecasturl)
+firstperiod=$(echo $forecast | jq '.properties.periods[0].name' | tr -d '"')
+echo done: $firstperiod done2
+detailedForecast=$(echo $forecast | jq '.properties.periods[0].detailedForecast' | tr -d '"')
 echo $firstperiod"'s Forecast for ("$mylatitude","$mylongitude"):" $detailedForecast
 
 users
